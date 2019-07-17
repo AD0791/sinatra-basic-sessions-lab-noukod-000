7558ca86a9d5583bcd6f1203f9bb3f52240cfec7
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-17 15:54:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    # create session la
+    enable :sessions
+  end
+
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # data are in params and session. 2 hashes
+    @item = params['item']
+    session['item'] = params['item']
+    @session = session
+    #binding.pry
+    erb :checkout
+  end
 end
