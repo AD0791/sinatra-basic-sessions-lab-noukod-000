7e74c708982f16734ed9f394e4611b3cb7aa750e
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-17 15:39:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
-    @session = session
-    erb :index
-  end
 
   configure do
     enable: sessions
+  end
+
+
+  get '/' do
+    @session = session
+    erb :index
   end
 
   post '/checkout' do
